f270a3bfe552e5ce64473ac6ff53187f12c27255
Scott Moore <scott.will.moore@gmail.com>
2018-07-22 17:27:08 +1000

    Write notes for problem 39

--- 0039/0039.py
+++ 0039/0039.py
@@ -3,6 +3,12 @@ from math import sqrt
 perfect_squares = {n ** 2 for n in range(1, 1000)}
 pythagorean_tripes = {}
 
+# a < b < c
+# a + b + c <= 1000
+# a + b + sqrt(a ** 2 + b ** 2) <= 1000
+# :: a < 1000
+# :: b < 1000 * (a - 500) / (a - 1000)
+
 for a in range(1, 1000):
     b_max = 1000 * (a - 500) // (a - 1000)
     for b in range(a, b_max):
