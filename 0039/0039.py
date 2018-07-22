13b6d994c0ba35959943add5d4c448b7d91793a4
Scott Moore <scott.will.moore@gmail.com>
2018-07-22 17:23:21 +1000

    Complete problem 39

---
+++ 0039/0039.py
@@ -0,0 +1,17 @@
+from math import sqrt
+
+perfect_squares = {n ** 2 for n in range(1, 1000)}
+pythagorean_tripes = {}
+
+for a in range(1, 1000):
+    b_max = 1000 * (a - 500) // (a - 1000)
+    for b in range(a, b_max):
+        c_2 = a ** 2 + b ** 2
+        if c_2 in perfect_squares:
+            c = int(sqrt(a ** 2 + b ** 2))
+            p = a + b + c
+            pythagorean_tripes[p] = pythagorean_tripes.get(p, []) + [(a, b, c)]
+
+perimeter_counts = {p: len(triples) for p, triples in pythagorean_tripes.items()}
+result = max(perimeter_counts, key=perimeter_counts.get)
+print(result)
